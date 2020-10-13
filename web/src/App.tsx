ba6eacb43fad1f9d16efecb631956bb234db57a8
devbacega <dev.victor.bacega@gmail.com>
2020-10-13 04:33:10 -0300

    Landing e Map construidos.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-
+import './styles/global.css';
+import Routes from './routes';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello World NLW
-        </h1>
-      </header>
-    </div>
+   <Routes />
   );
 }
 
